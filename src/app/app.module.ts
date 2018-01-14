0e9284e5366639d1b119fd9f1e5225cbc873a836
Naumand Napuce <nnapuce@helius-software.com>
2018-01-14 01:25:42 +0100

    Disa ndryshime

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule  } from '@angular/http';
+import { MatDialog } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MatDialog,
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
