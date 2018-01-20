b3acfc736437444587d0fb76088aeb2da0358549
Naumand Napuce <nnapuce@helius-software.com>
2018-01-20 17:18:52 +0100

    promises part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,14 +18,14 @@ export class HomeComponent implements OnInit {
 dish :Dish;
 promotion:Promotion;
 leader: Leader;
-  constructor(private dishservice:DishService,
-  private promotionservice:PromotionService,
+constructor(private dishservice:DishService,
+private promotionservice:PromotionService,
 private leaderservice:LeaderService) { }
 
   ngOnInit() {
-    this.dish=this.dishservice.getFeaturedDish();
-    this.promotion = this.promotionservice.getFeaturedPromotion();
-    this.leader=this.leaderservice.getFeaturedLeader();
+    this.dishservice.getFeaturedDish().then(dish=>this.dish=dish);
+    this.promotionservice.getFeaturedPromotion().then(promotion=>this.promotion=promotion);
+    this.leaderservice.getFeaturedLeader().then(leader=>this.leader=leader);
   }
 
 }
